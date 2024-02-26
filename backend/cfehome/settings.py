96404bca89f2331bcb501030ebf550b850428915
Abdelrhman Mamdouh <a.mammdouh97@gmail.com>
2024-02-26 22:54:34 +0200

    pagination

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -137,5 +137,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PERMISSIONS_CLASSES":[
         'rest_framework.permessions.IsAuthenticatedOrReadOnly'
-    ]
+    ],
+    "DEFAULT_PAGINATION_CLASS": 'rest_framework.pagination.LimitOffsetPagination',
+    "PAGE_SIZE": 10
 }
